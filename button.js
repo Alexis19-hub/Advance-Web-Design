e6de9203feb42270b2751c7189ba259dbb6a1887
Alexis Jan Wayne Latayan <80021819+Alexis19-hub@users.noreply.github.com>
2024-01-19 13:23:58 +0800

    Add files via upload

---
+++ button.js
@@ -0,0 +1,9 @@
+let result = document.getElementById("GFG");
+ 
+        function changeColor(color) {
+            document.body.style.background = color;
+        }
+ 
+        function myFunc() {
+            changeColor('green');
+        }        
